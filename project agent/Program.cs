b1ea7d74b3c31fbb248a5c74b18c327643af58a5
school <school@A45PC9>
2025-02-25 09:57:04 +0200

    add menu

--- project agent/Program.cs
+++ project agent/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
-
 Console.WriteLine("Здоров агент обміну і продажу нерухомості");
+Console.WriteLine("-----MENU-----");
 Console.WriteLine("Заповнення бази даних");
-Console.WriteLine("Перегляд даних про всі заявки");
- 
+Console.WriteLine("Перегляд даних про всі заявки");;
+Console.WriteLine("Доповнення бази даних");
+Console.WriteLine("Видалення із бази даних")
